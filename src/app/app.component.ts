4dbfca7af844ef8c8e7780f3882a7e932da7a01e
Hugo Nicolli <hugo.soares1@wipro.com>
2018-05-24 10:11:35 -0300

    create language options. button*2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,15 @@ export class AppComponent {
   title = 'Tour of Heroes';
   param = {value: 'world'};
 
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService) {
       // this language will be used as a fallback when a translation isn't found in the current language
       translate.setDefaultLang('en');
 
        // the lang to use, if the lang isn't available, it will use the current loader to get them
       translate.use('en');
+  }
+
+  setLang(lang: string) {
+    this.translate.use(lang);
   }
 }
